4189e3c2d62d21fcc3f508402e7c63751cb99c35
Daniick56 <danielballesteros22@outlook.com>
2024-07-24 12:09:14 -0500

    Finalizacion generacion de la eleccion de la máquina

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const divsCeldas = document.querySelectorAll(".cell");
 
+let rowChooseMachine = 0;
+let columnChooseMachine = 0;
+
 const lockersList = [
 
     [divsCeldas[0],  divsCeldas[1],  divsCeldas[2]],
@@ -24,17 +27,44 @@ const lockersList = [
             //         const element = lockersList[indexRow][indexColumn];
             //         console.log(element.textContent);
             //     }
-            // }           
+            // }
 
-for (let indexRow = 0; indexRow < lockersList.length; indexRow++) {
-    for (let indexColumn = 0; indexColumn < lockersList[indexRow].length; indexColumn++) {
-        const element = lockersList[indexRow][indexColumn];
-        element.addEventListener('click', ()=> {
-            if(isEmpty(lockersList, indexRow, indexColumn)) {
-                element.textContent = 'X';
-            }
-        });
+
+//Implementacion de flujo: 
+
+handleClick();
+
+function handleClick() {
+    for (let indexRow = 0; indexRow < lockersList.length; indexRow++) {
+        for (let indexColumn = 0; indexColumn < lockersList[indexRow].length; indexColumn++) {
+            const element = lockersList[indexRow][indexColumn];
+            element.addEventListener('click', ()=> {
+                if(isEmpty(lockersList, indexRow, indexColumn)) {
+                    element.textContent = 'X';
+                }
+                machineTurn();
+                verifyWinner();//IMPLEMENTAR
+            });
+        }
     }
+}
+
+function machineTurn () {
+    do {
+        rowChooseMachine = getRandomNumber(0, 2);
+        columnChooseMachine = getRandomNumber(0, 2);
+    } while (!isEmpty(lockersList, rowChooseMachine, columnChooseMachine));
+
+    lockersList[rowChooseMachine][columnChooseMachine].textContent = 'O';
+}
+
+function verifyWinner() {
+
+}
+
+//funcion que retorna un numero aleatorio.
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 function isEmpty(matriz, rowIndex, columnIndex) {
