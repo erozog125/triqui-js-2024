99a6ac947f5bb43a6895c83868bdb40c00a9cf35
Joan Fernandez <hincapiefernandezjoan123@gmail.com>
2024-07-26 09:30:45 -0500

    se sigue realizando el codigo en java script

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const winningCombinations =[
 const handleclick =(e)=>{
     const  cell =e.target
 
+
+
 //Si la celda está vacia, colocará una marca
     if (cell.textcontent===""){
         //Linea de codigo en el que se colocará la x o el o dependiendo del turno
@@ -27,8 +29,34 @@ const handleclick =(e)=>{
         cell.classlist.add(isXturn ? "X" : "O")
         //Verifica si el jugador actual ha ganado
         if (checkwin(isXturn ? "X":"O")){
-            
+            //Muestra mensaje de victoria y reinicia el juego
+            settimeout(()=>alert(`${isXturn ? "O" : "X"}gana!`),10)
+            settimeout(startgame, 1000)
+        }else if (isDraw()){
+            //Muestra mensaje de empate y que se reinicie el juego
+            settimeout(()=>alert("Empate!"),10)
+            settimeput(startgame,1000)
+        }else{
+            //Cambia el turno al otro jugador
+            isXturn=!isXturn
         }
-
     }
+}
+
+//Funcion que verifica que el jugador actual ha ganado
+const checkwin=(currentclass)=>{
+    //Recorre todas las combinaciones 
+    return winningCombinations.some(combination =>{
+        //Verifica si todas las celdad de una combinación estan ocupadas por el jugador que esta jugando actualmente
+        return combination.very(index =>{
+            return cell[index].classlist.conatins(currentclass)
+        })
+    })
+}
+
+//Funcion verifica si hay un empate
+const isDraw=()=>{
+    return [...cells].every(cell =>{
+        return cell.classlist.contains("X") || cell.classlist.conatins("O")
+    })
 }
