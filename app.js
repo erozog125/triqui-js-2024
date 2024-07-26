81e7e253f34cf2c6db9455d2e9b654056bf8640a
Daniick56 <danielballesteros22@outlook.com>
2024-07-26 09:09:09 -0500

    Añadiendo verificacion de empate

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const divsCeldas = document.querySelectorAll(".cell");
+const divsCeldas = document.querySelectorAll(".cell"); //nos traemos a todas las celdas del html
 
+// variables para guardar la elección de la máquina
 let rowChooseMachine = 0;
 let columnChooseMachine = 0;
 
+// creamos una matriz para guardar cada celda del html
 const lockersList = [
 
     [divsCeldas[0],  divsCeldas[1],  divsCeldas[2]],
@@ -12,11 +14,11 @@ const lockersList = [
     [divsCeldas[6],  divsCeldas[7],  divsCeldas[8]]
 
 ];
-
+// creamos otra matriz para manejar los estados de cada celda
 const statusLockers = [
-    ["",  "",  ""],
-
-    ["",  "",  ""],
+    ["",  "",  ""],         // "" es vacio
+                            // x  es usuario
+    ["",  "",  ""],         // o  es máquina
 
     ["",  "",  ""]
 ];
@@ -41,6 +43,7 @@ const statusLockers = [
 //Implementacion de flujo: 
 handleClick();
 
+//función que recorre la matriz, verifica que la celda esté vacia y crea el evento de click
 function handleClick() {
     for (let indexRow = 0; indexRow < lockersList.length; indexRow++) {
         for (let indexColumn = 0; indexColumn < lockersList[indexRow].length; indexColumn++) {
@@ -59,6 +62,7 @@ function handleClick() {
     }
 }
 
+//función que genera dos numeros aleatorios para la fila y la columna
 function machineTurn () {
     do {
         rowChooseMachine = getRandomNumber(0, 2);
@@ -70,6 +74,7 @@ function machineTurn () {
     statusLockers[rowChooseMachine][columnChooseMachine] = 'o'
 }
 
+// función que verifica los posibles escenarios del juego
 function verifyWinner() {
      //Verificar todas las posibles combinaciones ganadoras para el Usuario
     if ((statusLockers[0][0] === 'x' && statusLockers[0][1] === 'x' && statusLockers[0][2] === 'x') ||
@@ -95,17 +100,36 @@ function verifyWinner() {
         showWinner("¡Perdiste! 😞");
         return;
     }
+    if (verifyFullTable()) {
+        showWinner("¡Es un empate! 🤜🏼🤛🏼");
+    }
 }
 
-//funcion que retorna un numero aleatorio.
+//función que retorna un numero aleatorio.
 function getRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+// función que retorna true si la casilla se encuentra vacía
 function isEmpty(matriz, rowIndex, columnIndex) {
     return matriz[rowIndex][columnIndex].textContent === ''? true : false;
 }
 
+// función que verifica que la tabla esté completamente llena
+function verifyFullTable() {
+    let counter = 0;
+    statusLockers.forEach((row, rowIndex) => {
+        row.forEach((cell, columnIndex) => {
+            if (!isEmpty(lockersList, rowIndex, columnIndex)) {
+                counter++;
+            }
+        });
+    });
+    console.log(counter===9)
+    return counter===9;
+}
+
+// función que envia una alerta personalizada
 function showWinner(message) {
     Swal.fire({
         title: message,
@@ -116,17 +140,7 @@ function showWinner(message) {
     });
 }
 
-function verifyFullTable() {
-    let counter = 0;
-    statusLockers.forEach((row, rowIndex) => {
-        row.forEach((cell, columnIndex) => {
-            if (!isEmpty(lockersList, rowIndex, columnIndex)) {
-                counter++;
-            }
-        });
-    });
-    return counter===9;
-}
+// función que resetea el juego
 function reloadGame() {
     rowChooseMachine = 0;
     columnChooseMachine = 0;
