380ed23dbfa23c220a6aa324232fa5b34949221d
zowwii <cristiancmgallego@gmail.com>
2024-07-26 18:25:25 -0500

    hide selector

--- apps.js
+++ apps.js
@@ -1,8 +1,9 @@
 const board = document.querySelectorAll('.cell')
-let playUser = document.getElementById('selection')
+let playUser = document.getElementById('selection') 
 
-board[0].addEventListener('click',chooseletter)
-board[1].addEventListener('click',chooseboard1)
+
+board[0].addEventListener('click',chooseletter,)
+board[1].addEventListener('click',chooseboard1,)
 board[2].addEventListener('click',chooseboard2)
 board[3].addEventListener('click',chooseboard3)
 board[4].addEventListener('click',chooseboard4)
@@ -14,28 +15,38 @@ board[8].addEventListener('click',chooseboard8)
 
 function chooseletter(){
     board[0].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard1(){
     board[1].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard2(){
     board[2].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard3(){
     board[3].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard4(){
     board[4].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard5(){
     board[5].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard6(){
     board[6].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard7(){
     board[7].textContent = playUser.value
+    playUser.style.display = 'none'
 }
 function chooseboard8(){
     board[8].textContent = playUser.value
+    playUser.style.display = 'none'
 }
+
