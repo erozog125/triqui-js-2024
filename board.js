1c667da4d5aefa0705185fbcbe23601fe81e9eff
zowwii <cristiancmgallego@gmail.com>
2024-07-27 09:34:14 -0500

    eleccion del jugador

---
+++ board.js
@@ -0,0 +1,27 @@
+const board = document.querySelectorAll('.cell')
+const modal = document.getElementById('modal')
+const optX = document.getElementById('btn-x')
+const optO = document.getElementById('btn-o')
+const resultMessage = document.getElementById('result-message') // Añade un elemento para mostrar el resultado
+let user = ''
+let machine = ''
+let boardGame = ['', '', '', '', '', '', '', '', '']
+let isGameOver = false
+
+optX.addEventListener('click',() => {
+    user = 'X'
+    machine = 'O'
+    closeModal ()
+    playUser()
+}  )
+
+optO.addEventListener('click',() => {
+    user = 'O'
+    machine = 'X'
+    closeModal ()
+    playUser()
+}  )
+
+function closeModal(){
+    modal.style.display = 'none'
+}
