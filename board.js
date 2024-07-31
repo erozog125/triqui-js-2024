5f5856a65ded0ebd490e283f8704476af9b44224
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-07-30 22:53:04 -0500

    modal

--- board.js
+++ board.js
@@ -8,9 +8,33 @@ let machine = ''
 let boardGame = ['', '', '', '', '', '', '', '', '']
 let isGameOver = false
 
+
+function userOption(option){
+  user = option;
+  machine = option === 'X' ? 'O' : 'X';
+  modal.style.display = 'none';  
+  console.log(option);
+  console.log(machine);
+}
+
 function playUser() {
-    board.forEach((cell, idx) => {
-      cell.addEventListener('click', handleUserClick.bind(null, idx));
-    });
-  }
+  board.forEach((cell, idx) => {
+    cell.addEventListener('click', handleUserClick.bind(null, idx));
+  });
+}
+
+optX.addEventListener('click', () => {
+  userOption('X')
+  playUser()
+});
+optO.addEventListener('click', () =>  {
+  userOption('O')
+  playUser()
+});
+
+
+
+
+
+
   
