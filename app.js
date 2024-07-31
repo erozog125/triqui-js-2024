9b2390f047bf6bc1e47fd00ee71c35f82921e4fe
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-07-31 10:04:38 -0500

    Se agrego metodo para validar las combinaciones con las que se puede ganar el juego

--- app.js
+++ app.js
@@ -73,3 +73,26 @@ function validateGame() {
         console.log("Pendiente")
     }
 }
+
+function checkWinner() {
+    const winningCombinations = [
+        [0,1,2]
+        [3,4,5]
+        [6,7,8]
+        [0,3,6]
+        [1,4,7]
+        [2,5,8]
+        [0,4,8]
+        [2,4,6]
+    ]
+
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (boardGame[a] === boardGame[b] &&
+            boardGame[b] === boardGame[c] &&
+            boardGame[a] !== "") {
+            return boardGame[a];
+        }
+    }
+    return null;
+}
