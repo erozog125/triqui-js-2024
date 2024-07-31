6b7a5b729ccaea86b4e82854d0762e667acf8ae6
zowwii <cristiancmgallego@gmail.com>
2024-07-30 21:41:38 -0500

    implementacion notion

--- board.js
+++ board.js
@@ -8,20 +8,99 @@ let machine = ''
 let boardGame = ['', '', '', '', '', '', '', '', '']
 let isGameOver = false
 
-optX.addEventListener('click',() => {
-    user = 'X'
-    machine = 'O'
-    closeModal ()
-    playUser()
-}  )
-
-optO.addEventListener('click',() => {
-    user = 'O'
-    machine = 'X'
-    closeModal ()
-    playUser()
-}  )
-
-function closeModal(){
+function closeModal(){ //metodo cierra la ventana al seleccionar X o O
     modal.style.display = 'none'
+}
+
+optX.addEventListener('click', ()=> { // boton de seleccion de X del jugador
+    user='X'
+    machine='O'
+    closeModal()
+    playerUser()
+})
+
+optO.addEventListener('click', ()=> { // boton de seleccion de O del jugador
+    user='O'
+    machine='X'
+    closeModal()
+    playerUser()
+})
+
+function playerUser(){ //metodo para que acepte el clik y coloque en la celda el simbolo seleccionado
+    board.forEach((cell, idx)=> { // recorre el comando por cada celda y obtiene el indice
+        cell.addEventListener('click',handleUserClick.bind(null, idx));
+    });
+}
+
+function playMachine(){
+    let opMachine
+    do{
+        opMachine = getRandomNumber()
+    } while (boardGame[opMachine] !== '' || isGameOver)
+
+        board[opMachine].textContent = machine
+        boardGame[opMachine] = machine
+        validateGame()
+}
+
+function handleUserClick(idx){
+    if(!isGameOver && board[idx].textContent === ''){
+        board[idx].textContent = user
+        boardGame[idx] = user
+        validateGame()
+        if(!isGameOver){
+            setTimeout(()=>{ //el settimeout espera un 1 segundo para que la maquina juegue
+                playMachine()
+            }, 1000)
+        }
+    }
+}
+
+function validateGame(){
+    const winner = checkWinner()
+    if(winner) {
+        showResult(`${winner} gana!`)
+        isGameOver = true
+        board.forEach((cell)=>{
+            cell.removeEventListener('click', handleUserClick)
+        })
+    }else if(!boardGame.includes('')){
+        showResult('Empate!')
+        isGameOver = true
+        board.forEach((cell)=>{
+            cell.removeEventListener('click', handleUserClick)
+        })
+    }else{
+        console.log('pendiente')
+    }
+}
+
+function checkWinner(){
+    const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+    for(const combination of winningCombinations){
+        const [a,b,c] = combination
+        if (boardGame[a] === boardGame[b] &&
+            boardGame[b] === boardGame[c] &&
+            boardGame[a] != ''){
+            return boardGame[a]
+        }
+    }
+    return null
+}
+
+function showResult(message){
+    Swal.fire(message)
+}
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 9)
 }
